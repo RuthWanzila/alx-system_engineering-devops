86f341e4c62acb8846dc874ae9c56d11b76af4a1
RuthWanzila <wanzilaruth533@gmail.com>
2024-02-19 02:03:54 -0800

    export to SON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # Get the employee ID from command-line argument
+    employee_id = sys.argv[1]
+
+    # API URL
+    url = "https://jsonplaceholder.typicode.com/"
+
+    # Fetch user information
+    user_response = requests.get(url + "users/{}".format(employee_id))
+    user = user_response.json()
+    username = user.get("username")
+
+    # Fetch TODO list for the employee
+    todos_resp = requests.get(url + "todos", params={"userId": employee_id})
+    todos = todos_resp.json()
+
+    # Export TODO list to JSON
+    with open("{}.json".format(employee_id), "w") as jsonfile:
+        json.dump(
+            {
+                employee_id: [
+                    {
+                        "task": task.get("title"),
+                        "completed": task.get("completed"),
+                        "username": username,
+                    }
+                    for task in todos
+                ]
+            },
+            jsonfile,
+        )
