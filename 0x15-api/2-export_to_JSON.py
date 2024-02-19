396f257cfabf8080502555aaa5de9dcea67f77ca
RuthWanzila <wanzilaruth533@gmail.com>
2024-02-19 08:59:25 -0800

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,41 +1,19 @@
 #!/usr/bin/python3
 """ export data in the JSON format """
 import json
+import requests
+import sys
 
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-def export_tasks_to_json(user_id, tasks):
-    data = {user_id: []}
-    for task in tasks:
-        task_data = {
-            'task': task['title'],
-            'completed': task['completed'],
-            'username': task['username']
-        }
-        data[user_id].append(task_data)
-
-    filename = f'{user_id}.json'
-    with open(filename, 'w') as file:
-        json.dump(data, file)
-
-
-# Example usage
-user_id = 'USER123'
-tasks = [
-    {
-        'title': 'Task 1',
-        'completed': True,
-        'username': 'JohnDoe'
-    },
-    {
-        'title': 'Task 2',
-        'completed': False,
-        'username': 'JohnDoe'
-    },
-    {
-        'title': 'Task 3',
-        'completed': True,
-        'username': 'JohnDoe'
-    }
-]
-
-export_tasks_to_json(user_id, tasks)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
